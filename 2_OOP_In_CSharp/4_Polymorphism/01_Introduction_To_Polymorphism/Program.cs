49359ec5c85dae16de06ca421c1288524345bd6f
Robin Rahman <robin.rahman.nl@gmail.com>
2025-11-30 17:06:16 +0100

    chore: sync from mac

--- 2_OOP_In_CSharp/4_Polymorphism/01_Introduction_To_Polymorphism/Program.cs
+++ 2_OOP_In_CSharp/4_Polymorphism/01_Introduction_To_Polymorphism/Program.cs
@@ -3,7 +3,7 @@ Introduction to Polymorphism
 Imagine a method that changes its behavior based on the object it works with. In object-oriented programming, Polymorphism allows 
 methods
 Preview: Docs Methods are blocks of code that can be called elsewhere in the program.
- with the same name to perform different actions depending on the object.
+with the same name to perform different actions depending on the object.
 
 Consider an Animal class with subclasses like Dog, Cat, and Bird. Polymorphism lets you call the same method, such as MakeSound(), on any of these objects and get different results based on the subclass.
 
@@ -11,10 +11,10 @@ Without polymorphism, you’d need to check each Animal subclass type and call t
 
 Polymorphism provides several benefits:
 
-Flexibility and Maintainability: You can add new 
+Flexibility and Maintainability: You can add new []
 classes
 Preview: Docs A blueprint or template for creating objects.
- with little or no modification to the existing code.
+with little or no modification to the existing code.
 Code Reusability: Common functionality can be written in the base class, and specific functionality can be implemented in derived classes.
 Simplified Code: Complex switch-case or if-else structures can be avoided, making the code cleaner and easier to read.
 Instructions
